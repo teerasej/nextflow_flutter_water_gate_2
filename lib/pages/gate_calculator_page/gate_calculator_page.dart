21bae293afbb3124e232162b75edec46d956caf5
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-11-02 00:12:03 +0700

    แก้ไขพื้นที่ UI หลุดขอบด้วย ScrollView

--- lib/pages/gate_calculator_page/gate_calculator_page.dart
+++ lib/pages/gate_calculator_page/gate_calculator_page.dart
@@ -9,110 +9,112 @@ class GateCalculatorPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('คำนวนประตูน้ำ'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'จำลองระดับน้ำเหนือเขื่อน (m MSL)',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'จำลองระดับน้ำเหนือเขื่อน (m MSL)',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'ระดับน้ำที่ต้องการยกระดับ (m MSL)',
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'ระดับน้ำที่ต้องการยกระดับ (m MSL)',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'แผนระบายน้ำ (m MSL)',
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'แผนระบายน้ำ (m MSL)',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Total Sum SNR Release (MCM)',
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Total Sum SNR Release (MCM)',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Total Sum TTN Release (MCM)',
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Total Sum TTN Release (MCM)',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'ปั้มสูบกลับ (m MSL)',
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'ปั้มสูบกลับ (m MSL)',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text('Unit 1 (80)'),
-            Slider(
-              value: 80,
-              min: 80,
-              max: 120,
-              divisions: 4,
-              onChanged: (double value) {
-                print(value);
-              },
-            ),
-            Text('Unit 2 (80)'),
-            Slider(
-              value: 80,
-              min: 80,
-              max: 120,
-              divisions: 4,
-              onChanged: (double value) {
-                print(value);
-              },
-            ),
-            Text('Unit 3 (80)'),
-            Slider(
-              value: 80,
-              min: 80,
-              max: 120,
-              divisions: 4,
-              onChanged: (double value) {
-                print(value);
-              },
-            ),
-            Text('Unit 4 (150)'),
-            Slider(
-              value: 150,
-              min: 150,
-              max: 180,
-              divisions: 3,
-              onChanged: (double value) {
-                print(value);
-              },
-            ),
-            Text('Unit 5 (150)'),
-            Slider(
-              value: 150,
-              min: 150,
-              max: 180,
-              divisions: 3,
-              onChanged: (double value) {
-                print(value);
-              },
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('คำนวน'),
+              SizedBox(
+                height: 16,
               ),
-            ),
-          ],
+              Text('Unit 1 (80)'),
+              Slider(
+                value: 80,
+                min: 80,
+                max: 120,
+                divisions: 4,
+                onChanged: (double value) {
+                  print(value);
+                },
+              ),
+              Text('Unit 2 (80)'),
+              Slider(
+                value: 80,
+                min: 80,
+                max: 120,
+                divisions: 4,
+                onChanged: (double value) {
+                  print(value);
+                },
+              ),
+              Text('Unit 3 (80)'),
+              Slider(
+                value: 80,
+                min: 80,
+                max: 120,
+                divisions: 4,
+                onChanged: (double value) {
+                  print(value);
+                },
+              ),
+              Text('Unit 4 (150)'),
+              Slider(
+                value: 150,
+                min: 150,
+                max: 180,
+                divisions: 3,
+                onChanged: (double value) {
+                  print(value);
+                },
+              ),
+              Text('Unit 5 (150)'),
+              Slider(
+                value: 150,
+                min: 150,
+                max: 180,
+                divisions: 3,
+                onChanged: (double value) {
+                  print(value);
+                },
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('คำนวน'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
