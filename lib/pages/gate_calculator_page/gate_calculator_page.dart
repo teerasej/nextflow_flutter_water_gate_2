e239b0b2fd15f769184f5aa62282d620042351ba
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-11-02 00:28:31 +0700

    เชื่อมค่าของ unit เข้ากับ slider ตัวแรก - ใช้ obx widget เพื่อให้อัพเดตทุกครั้งที่ตัวแปร obs มีการเปลี่ยน

--- lib/pages/gate_calculator_page/gate_calculator_page.dart
+++ lib/pages/gate_calculator_page/gate_calculator_page.dart
@@ -15,136 +15,139 @@ class GateCalculatorPage extends StatelessWidget {
         title: Text('คำนวนประตูน้ำ'),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'จำลองระดับน้ำเหนือเขื่อน (m MSL)',
-                ),
-                controller: controller.ttn_for_textCtrl.value,
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  controller.ttn_for = double.parse(value);
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'ระดับน้ำที่ต้องการยกระดับ (m MSL)',
-                ),
-                controller: controller.lv_inp_textCtrl.value,
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  controller.lv_inp = double.parse(value);
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'แผนระบายน้ำ (m MSL)',
-                ),
-                controller: controller.demand_irr_textCtrl.value,
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  controller.demand_irr = double.parse(value);
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Total Sum SNR Release (MCM)',
-                ),
-                controller: controller.snr_rrel_textCtrl.value,
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  controller.snr_rrel = double.parse(value);
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Total Sum TTN Release (MCM)',
-                ),
-                controller: controller.ttnsumrel_textCtrl.value,
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  controller.ttnsumrel = double.parse(value);
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'ปั้มสูบกลับ (m MSL)',
-                ),
-                controller: controller.dis_tol_textCtrl.value,
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  controller.dis_tol = double.parse(value);
-                },
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Text('Unit 1 (80)'),
-              Slider(
-                value: 80,
-                min: 80,
-                max: 120,
-                divisions: 4,
-                onChanged: (double value) {
-                  print(value);
-                },
-              ),
-              Text('Unit 2 (80)'),
-              Slider(
-                value: 80,
-                min: 80,
-                max: 120,
-                divisions: 4,
-                onChanged: (double value) {
-                  print(value);
-                },
-              ),
-              Text('Unit 3 (80)'),
-              Slider(
-                value: 80,
-                min: 80,
-                max: 120,
-                divisions: 4,
-                onChanged: (double value) {
-                  print(value);
-                },
-              ),
-              Text('Unit 4 (150)'),
-              Slider(
-                value: 150,
-                min: 150,
-                max: 180,
-                divisions: 3,
-                onChanged: (double value) {
-                  print(value);
-                },
-              ),
-              Text('Unit 5 (150)'),
-              Slider(
-                value: 150,
-                min: 150,
-                max: 180,
-                divisions: 3,
-                onChanged: (double value) {
-                  print(value);
-                },
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    controller.calculate();
+        child: Obx(
+          () => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'จำลองระดับน้ำเหนือเขื่อน (m MSL)',
+                  ),
+                  controller: controller.ttn_for_textCtrl.value,
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    controller.ttn_for = double.parse(value);
                   },
-                  child: const Text('คำนวน'),
                 ),
-              ),
-            ],
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'ระดับน้ำที่ต้องการยกระดับ (m MSL)',
+                  ),
+                  controller: controller.lv_inp_textCtrl.value,
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    controller.lv_inp = double.parse(value);
+                  },
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'แผนระบายน้ำ (m MSL)',
+                  ),
+                  controller: controller.demand_irr_textCtrl.value,
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    controller.demand_irr = double.parse(value);
+                  },
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Total Sum SNR Release (MCM)',
+                  ),
+                  controller: controller.snr_rrel_textCtrl.value,
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    controller.snr_rrel = double.parse(value);
+                  },
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Total Sum TTN Release (MCM)',
+                  ),
+                  controller: controller.ttnsumrel_textCtrl.value,
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    controller.ttnsumrel = double.parse(value);
+                  },
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'ปั้มสูบกลับ (m MSL)',
+                  ),
+                  controller: controller.dis_tol_textCtrl.value,
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    controller.dis_tol = double.parse(value);
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text('Unit 1 (${controller.u1Value.value})'),
+                Slider(
+                  value: controller.u1Value.value,
+                  min: 80,
+                  max: 120,
+                  divisions: 4,
+                  label: controller.u1Value.value.toString(),
+                  onChanged: (double value) {
+                    controller.u1Value.value = value;
+                  },
+                ),
+                Text('Unit 2 (80)'),
+                Slider(
+                  value: 80,
+                  min: 80,
+                  max: 120,
+                  divisions: 4,
+                  onChanged: (double value) {
+                    print(value);
+                  },
+                ),
+                Text('Unit 3 (80)'),
+                Slider(
+                  value: 80,
+                  min: 80,
+                  max: 120,
+                  divisions: 4,
+                  onChanged: (double value) {
+                    print(value);
+                  },
+                ),
+                Text('Unit 4 (150)'),
+                Slider(
+                  value: 150,
+                  min: 150,
+                  max: 180,
+                  divisions: 3,
+                  onChanged: (double value) {
+                    print(value);
+                  },
+                ),
+                Text('Unit 5 (150)'),
+                Slider(
+                  value: 150,
+                  min: 150,
+                  max: 180,
+                  divisions: 3,
+                  onChanged: (double value) {
+                    print(value);
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      controller.calculate();
+                    },
+                    child: const Text('คำนวน'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
