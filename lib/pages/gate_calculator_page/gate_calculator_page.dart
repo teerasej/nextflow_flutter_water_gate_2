d7866ab0fda047c1bce42f0502ace996b0654309
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-11-01 23:59:49 +0700

    ใส่ช่องกรอกข้อมูลที่เหลือ และใส่ padding

--- lib/pages/gate_calculator_page/gate_calculator_page.dart
+++ lib/pages/gate_calculator_page/gate_calculator_page.dart
@@ -9,15 +9,48 @@ class GateCalculatorPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('คำนวนประตูน้ำ'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'จำลองระดับน้ำเหนือเขื่อน (m MSL)',
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'จำลองระดับน้ำเหนือเขื่อน (m MSL)',
+              ),
+              keyboardType: TextInputType.number,
             ),
-            keyboardType: TextInputType.number,
-          ),
-        ],
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'ระดับน้ำที่ต้องการยกระดับ (m MSL)',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'แผนระบายน้ำ (m MSL)',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Total Sum SNR Release (MCM)',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Total Sum TTN Release (MCM)',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'ปั้มสูบกลับ (m MSL)',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ],
+        ),
       ),
     );
   }
